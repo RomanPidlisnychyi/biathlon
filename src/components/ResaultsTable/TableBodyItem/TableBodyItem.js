adf3e4f133d6b625b581715deba09a7383f10aec
RomanPidlisnychyi <myluckyhelper@gmail.com>
2021-03-28 06:53:25 +0300

    country image

--- src/components/ResaultsTable/TableBodyItem/TableBodyItem.js
+++ src/components/ResaultsTable/TableBodyItem/TableBodyItem.js
@@ -1,9 +1,11 @@
+import styles from './TableBodyItem.module.css';
+
 export default function TableBodyItem({ race, index }) {
   const { country, name, lie, stand, general, speed } = race;
   return (
     <tr>
       <td>{index + 1}</td>
-      <td>{country}</td>
+      <td className={styles[country]} />
       <td>{name}</td>
       <td>{lie}</td>
       <td>{stand}</td>
--- src/components/ResaultsTable/TableHeadItem/TableHeadItem.js
+++ src/components/ResaultsTable/TableHeadItem/TableHeadItem.js
@@ -1,11 +1,20 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { setFilter } from '../../../store/actions/filterActions';
 import { getFilter } from '../../../store/selectors/filterSelectors';
-// import {} from 'module';
+import styles from './TableHeadItem.module.css';
 
 export default function TableHeadItem({ title }) {
   const dispatch = useDispatch();
   const filter = useSelector(getFilter);
+  let newTitle;
+
+  if (title === 'lie' || title === 'stand' || title === 'general') {
+    newTitle = `${title} %`;
+  }
+
+  if (title === 'speed') {
+    newTitle = `${title} s.`;
+  }
 
   const handleBtn = () => {
     if (filter === title) {
@@ -16,8 +25,14 @@ export default function TableHeadItem({ title }) {
 
   return (
     <th>
-      <button type="button" onClick={handleBtn}>
-        {title}
+      <button
+        className={
+          title === filter ? `${styles.button} ${styles.active}` : styles.button
+        }
+        type="button"
+        onClick={handleBtn}
+      >
+        {newTitle ? newTitle : title}
       </button>
     </th>
   );
